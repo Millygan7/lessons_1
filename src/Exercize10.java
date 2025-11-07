9280b3f405a70eb3db86bd2fc9353f5e3957b416
Millygan7 <volodyan1923@gmail.com>
2025-11-07 12:01:52 +0300

    Update Exercize10.java

--- src/Exercize10.java
+++ src/Exercize10.java
@@ -1,16 +1,16 @@
 public class Exercize10 {
-    public static void main(String[] args) {
-        int[] numbers = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        numbers[0] = 0;
-        numbers[1] = 0;
-        numbers[2] = 1;
-        numbers[3] = 1;
-        numbers[4] = 0;
-        numbers[5] = 1;
-        numbers[6] = 0;
-        numbers[7] = 0;
-        numbers[8] = 1;
-        numbers[9] = 1;
-        for (int number : numbers) {
-            System.out.println(number); } } }
+        public static void main(String[] args) {
+            int[] numbers = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1};
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] == 0) {
+                    numbers[i] = 1;
+                } else {
+                    numbers[i] = 0;
+                }
+            }
+            for (int number : numbers) {
+                System.out.print(number + " ");
+            }
+        }
+    }
 
