30e274225150e8f6c3ff021653108b116cacc95d
Millygan7 <volodyan1923@gmail.com>
2025-11-07 12:04:33 +0300

    Update Exercize7.java

--- src/Exercize7.java
+++ src/Exercize7.java
@@ -1,12 +1,13 @@
 public class Exercize7 {
     static void main(String[] args) {
+        int value = -7;
         int a = -7;
-        if (a<0) {
+        if (value >= 0) {
 
-            System.out.println("True");
-        }
-        if (a>=0)  {
             System.out.println("False");
+        }
+        else   {
+            System.out.println("True");
         }
     }
 }
